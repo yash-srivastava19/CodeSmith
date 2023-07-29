555c7fec0bf1cd02a07461bf61892652704898e8
Yash Srivastava <85068689+yash-srivastava19@users.noreply.github.com>
2023-07-29 17:04:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# Contains main driver code for the Chainlit to work.
-
-from langchain import PromptTemplate, Cohere, LLMChain
+from langchain import PromptTemplate, LLMChain
 import chainlit as cl
-
-from custom_llm import CodeGenAlpha
+from custom_llm import CustomLLM
+from langchain.prompts import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+)
 
 template = """ Write a code for the following problem :
 {question} 
@@ -11,22 +12,33 @@ template = """ Write a code for the following problem :
 Code:
 """
 
-@cl.langchain_factory(use_async=False)
-def factory():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=CodeGenAlpha(), verbose=True)
-
-    return llm_chain
-
-
-@cl.langchain_rename   # This will be particularly useful when we want to customize this thing for production.
-def rename(orig_author):
-    rename_dict = {
-        'LLMChain': 'Scooby'   # Scooby is a great name for the LLM Chain !!
-    }
-    return rename_dict.get(orig_author, orig_author)
-
 
 @cl.on_chat_start
-async def main():
-    await cl.Message(content="Welcome to Cranberry !! Let's start this").send()
+def factory():
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
+    prompt = ChatPromptTemplate.from_messages([system_message_prompt])
+    llm = CustomLLM()
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True,)
+
+    cl.user_session.set("llm_chain", llm_chain)
+
+
+
+@cl.on_message
+async def main(message):
+    llm_chain = cl.user_session.get("llm_chain")
+
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+
+    await cl.Message(content=res["text"]).send()
+
+
+
+@cl.author_rename   # This will be particularly useful when we want to customize this thing for production.
+def rename(orig_author):
+    rename_dict = {
+        'LLMChain': 'Scooby'
+    }
+    return rename_dict.get(orig_author, orig_author)
